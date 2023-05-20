f635b719f59de978c45444f6f5dde9d975ba74fb
Paweł Jamroziak <pjamroziak@gmail.com>
2023-05-20 16:19:25 +0000

    fix: use controller and endpoint path

--- apps/api/src/app/routes/scripts/script.controller.ts
+++ apps/api/src/app/routes/scripts/script.controller.ts
@@ -2,11 +2,11 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { SendMessageToClientsDto } from '@unsold-food-deals/schemas';
 import { ScriptService } from './script.service';
 
-@Controller()
+@Controller('scripts')
 export class ScriptController {
   constructor(private readonly service: ScriptService) {}
 
-  @Post()
+  @Post('send-message-to-clients')
   async sendMessageToClients(@Body() payload: SendMessageToClientsDto) {
     return this.service.sendMessageToClients(payload);
   }
