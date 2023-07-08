4b43bb497136da31779f5b55c1d5b38fa2569d22
Paweł Jamroziak <pjamroziak@gmail.com>
2023-07-08 08:30:55 +0000

    fix: disable client when user blocked bot

--- apps/telegram-bot/src/app/consumers/send-message/send-message.consumer.ts
+++ apps/telegram-bot/src/app/consumers/send-message/send-message.consumer.ts
@@ -5,17 +5,31 @@ import { Telegraf } from 'telegraf';
 import { Job } from 'bullmq';
 import { MessageSchema } from '@unsold-food-deals/schemas';
 import { DefaultParseMode } from '../../constants';
+import { ApiClient } from '@unsold-food-deals/api-client';
+
+type SendMessageTelegrafError = {
+  response: {
+    error_code: number;
+  };
+  on: {
+    payload: {
+      chat_id: string;
+    };
+  };
+};
 
 @Processor('send-message', {
   concurrency: 10,
 })
 export class SendMessageConsumer extends WorkerHost {
   private readonly logger = new Logger(SendMessageConsumer.name);
-  private readonly bot: Telegraf;
 
-  constructor(@InjectBot() bot: Telegraf) {
+  constructor(
+    @InjectBot()
+    private readonly bot: Telegraf,
+    private readonly apiClient: ApiClient
+  ) {
     super();
-    this.bot = bot;
   }
 
   async process(job: Job) {
@@ -34,8 +48,20 @@ export class SendMessageConsumer extends WorkerHost {
   }
 
   @OnWorkerEvent('failed')
-  onFailed(job, error) {
+  async onFailed(job, error) {
     this.logger.error({ error }, 'failed processing "send-message" job');
+    if (error?.response?.error_code === 403) {
+      const casted = error as SendMessageTelegrafError;
+
+      const { results } = await this.apiClient.client.find({
+        chatId: casted.on.payload.chat_id,
+      });
+
+      if (results.length > 0) {
+        const client = results[0];
+        await this.apiClient.client.update(client.id, { enabled: false });
+      }
+    }
   }
 
   @OnWorkerEvent('error')
