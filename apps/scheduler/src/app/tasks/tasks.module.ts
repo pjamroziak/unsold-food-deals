d4f6a568e78157ebc47207cec4b420dfd2181f68
Paweł Jamroziak <pjamroziak@gmail.com>
2023-09-05 18:07:22 +0000

    fix: replace old package method

--- apps/scheduler/src/app/tasks/tasks.module.ts
+++ apps/scheduler/src/app/tasks/tasks.module.ts
@@ -1,10 +1,13 @@
 import { BullModule } from '@nestjs/bullmq';
-import { Module } from '@nestjs/common';
+import { ConfigurableModuleAsyncOptions, Module } from '@nestjs/common';
 import { RequestScrapByCityTask } from './request-scrap-by-city.task';
 import { ApiConfig } from '../app.config';
-import { ApiClientModule } from '@unsold-food-deals/api-client';
+import {
+  ApiClientModule,
+  ApiClientOptions,
+} from '@unsold-food-deals/api-client';
 
-const apiClientFactory = {
+const apiClientFactory: ConfigurableModuleAsyncOptions<ApiClientOptions> = {
   inject: [ApiConfig],
   useFactory: (config: ApiConfig) => {
     return {
@@ -18,7 +21,7 @@ const apiClientFactory = {
     BullModule.registerQueue({
       name: 'requested-scrap-by-city',
     }),
-    ApiClientModule.registerAsync(apiClientFactory),
+    ApiClientModule.forRootAsync(apiClientFactory),
   ],
   providers: [RequestScrapByCityTask],
 })
